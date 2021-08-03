7cf252f6a8816fbb95202a40f00cf7b0edb5e9a6
phytter <phytter@hotmail.com>
2021-08-03 20:41:52 -0300

    feat: save

---
+++ src/main/index.tsx
@@ -0,0 +1,9 @@
+
+import ReactDOM from 'react-dom'
+import React from 'react'
+import Login from '../presentation/pages/login/index'
+
+ReactDOM.render(
+  <Login />,
+  document.getElementById('main')
+)
