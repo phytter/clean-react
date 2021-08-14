e02e3aa10829008ffac087c769f5c3a268314812
phytter <phytter@hotmail.com>
2021-08-14 16:20:38 -0300

    feat: ensure login start with submit button disabled

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -37,7 +37,7 @@ const Login: React.FC = () => {
             placeholder="Digite sua senha"
           />
 
-          <button className={Styles.submit} type="submit">Entrar</button>
+          <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
 
           <span className={Styles.link}>Criar conta</span>
           <FormStatus />
