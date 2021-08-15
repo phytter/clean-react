aa10e2cbd41895561ae737d031150b4b6a13e703
phytter <phytter@hotmail.com>
2021-08-15 09:23:46 -0300

    feat: ensure login call validation with correct email

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,10 +5,17 @@ import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
+  }
+
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setState(prev => ({
+      ...prev,
+      [event.target.name]: event.target.value
+    }))
   }
 
   const getStatus = (): string => {
@@ -18,9 +25,15 @@ const Input: React.FC<Props> = (props: Props) => {
   const getTitle = (): string => {
     return error
   }
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} readOnly onFocus={enableInput} />
+      <input
+        {...props}
+        data-testid={props.name}
+        readOnly onFocus={enableInput}
+        onChange={handleChange}
+      />
       <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>
         {getStatus()}
       </span>
