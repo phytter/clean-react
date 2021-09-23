2de5ab12f65241448321dbd646edb9a7386a675f
phytter <phytter@hotmail.com>
2021-09-23 20:29:06 -0300

    feat: ensure RemoteAddAccount throws EmailInUseError if HttpPostClient returns 403

--- src/data/protocols/http/http-response.ts
+++ src/data/protocols/http/http-response.ts
@@ -4,7 +4,8 @@ export enum HttpStatusCode {
   badRequest = 400,
   unauthorized = 401,
   notFound = 404,
-  serverError = 500
+  serverError = 500,
+  forbidden = 403
 }
 
 export type HttpResponse<T> = {
