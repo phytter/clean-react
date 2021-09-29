0e1425d5a595a20fd34db087237eba351462cb86
phytter <phytter@hotmail.com>
2021-09-29 20:33:14 -0300

    refactor: change Validation to receive an object instead of value

--- src/validation/test/mock-field-validation.ts
+++ src/validation/test/mock-field-validation.ts
@@ -4,7 +4,7 @@ export class FieldValidationSpy implements FieldValidation {
   error: Error = null
   constructor (readonly field: string) {}
 
-  validate (valie: string): Error {
+  validate (input: object): Error {
     return this.error
   }
 }
--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,8 +4,9 @@ import { InvalidFieldError } from '@/validation/errors'
 export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
-  validate (value: string): Error {
+  validate (input: object): Error {
     const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    const value = input[this.field]
     return (!value || emailRegex.test(value)) ? null : new InvalidFieldError()
   }
 }
