ef74eaccd2fa278059abe56d315f1f8a99f71477
phytter <phytter@hotmail.com>
2021-10-02 17:15:58 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('/login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.get('[data-testid="email-status"]')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴️')
+    cy.get('[data-testid="password-status"]')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴️')
+    cy.get('[data-testid="submit"]').should('have.attr', 'disabled')
+    cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
   })
 })
