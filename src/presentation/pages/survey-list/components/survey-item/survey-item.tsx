31fc743acef0edc95cdc8529bb2ad6d362493a42
phytter <phytter@hotmail.com>
2021-11-24 19:55:07 -0300

    feat: ensure SurveyList render SurveyItems on success

--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -10,26 +10,24 @@ type Props = {
 const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
   const iconName = survey.didAnswer ? IconName.thumbUp : IconName.thumbDown
   return (
-    <div className={Styles.surveyItemWrap}>
-      <li>
-        <div className={Styles.surveyContent}>
-          <Icon className={Styles.iconWrap} iconName={iconName} />
-          <time>
-            <span data-testid="day" className={Styles.day}>
-              {survey.date.getDate().toString().padStart(2, '0')}
-            </span>
-            <span data-testid="month" className={Styles.month}>
-              {survey.date.toLocaleString('pt-BR', { month: 'short' }).replace('.', '')}
-            </span>
-            <span data-testid="year" className={Styles.year}>
-              {survey.date.getFullYear()}
-            </span>
-          </time>
-          <p data-testid="question">{survey.quentions}</p>
-        </div>
-        <footer>Ver Resultado</footer>
-      </li>
-    </div>
+    <li className={Styles.surveyItemWrap}>
+      <div className={Styles.surveyContent}>
+        <Icon className={Styles.iconWrap} iconName={iconName} />
+        <time>
+          <span data-testid="day" className={Styles.day}>
+            {survey.date.getDate().toString().padStart(2, '0')}
+          </span>
+          <span data-testid="month" className={Styles.month}>
+            {survey.date.toLocaleString('pt-BR', { month: 'short' }).replace('.', '')}
+          </span>
+          <span data-testid="year" className={Styles.year}>
+            {survey.date.getFullYear()}
+          </span>
+        </time>
+        <p data-testid="question">{survey.quentions}</p>
+      </div>
+      <footer>Ver Resultado</footer>
+    </li>
   )
 }
 
