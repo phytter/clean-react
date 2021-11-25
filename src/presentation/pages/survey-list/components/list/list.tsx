024f995f2cf92f80b09e978b7deabeba39c4d79d
phytter <phytter@hotmail.com>
2021-11-24 21:01:50 -0300

    refactor: move SurveyList code to components

---
+++ src/presentation/pages/survey-list/components/list/list.tsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+import { SurveyItem, SurveyItemEmpty, SurveyContext } from '@/presentation/pages/survey-list/components'
+import Styles from './list-styles.scss'
+import { SurveyModel } from '@/domain/models'
+
+const List: React.FC = () => {
+  const { state } = useContext(SurveyContext)
+  return (
+    <ul className={Styles.listWrap} data-testid="survey-list">
+      {state.surveys.length
+        ? state.surveys.map((survey: SurveyModel) => <SurveyItem key={survey.id} survey={survey} />)
+        : <SurveyItemEmpty />
+      }
+    </ul>
+  )
+}
+
+export default List
