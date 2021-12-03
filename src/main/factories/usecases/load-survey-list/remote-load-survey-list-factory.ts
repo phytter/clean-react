036d08960d64032cfdd109a9d0313dbead50b885
phytter <phytter@hotmail.com>
2021-12-03 19:20:56 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,8 +1,8 @@
 import { makeApiUrl } from '@/main/factories/http/api-url-factory'
 import { LoadSurveyList } from '@/domain/usecases'
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
 import { RemoteSurveyList } from '@/data/usecases'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+  return new RemoteSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpGetClientDecorator())
 }
